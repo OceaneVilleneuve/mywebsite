ea11593288702c7a6751c9d2b4c18122e837fc1a
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-11-09 17:57:11 +0100

    change name of one picture

--- src/pages/Portfolio/alexastudiocreation.jsx
+++ src/pages/Portfolio/alexastudiocreation.jsx
@@ -174,7 +174,7 @@ const dataMyWebsiteMobile = [
 const dataMyWebsite = [
   { imageWebsite: require('../../assets/acceuil-alexa.png'), },
   { imageWebsite:require('../../assets/menu.png'), },
-  { imageWebsite:require('../../assets/menu-déplie.png'), },
+  { imageWebsite:require('../../assets/menu-deplie.png'), },
   { imageWebsite:require('../../assets/mariage-photo.png'), },
   { imageWebsite:require('../../assets/video-mariage.png'), },
   { imageWebsite:require('../../assets/prise-rdv.png'), },
